234b38e119417fd3b4ed52ccb5f77cf9043cffd5
TT <3049720393@qq.com>
2017-07-27 13:45:43 +0800

    w

--- turnjs2/turn.js
+++ turnjs2/turn.js
@@ -1068,7 +1068,7 @@ turnMethods = {
 
 			e.stopPropagation();
 
-		if ((new Date().getTime())-data.time<200 || point.x<0 || point.x>$(this).width()) {
+		if ((new Date().getTime())-data.time<200 || point.x<0 || point.x>$(this).width()/2) {
 			e.preventDefault();
 			data.opts.turn.data().tpage = data.opts.next;
 			data.opts.turn.turn('update');
